4fdce8e4987041563dbae087d5dff809d648186d
newroadbike <102960859+newroadbike@users.noreply.github.com>
2022-05-04 12:18:45 +0200

    task_07

---
+++ code/task-07/sketch.js
@@ -0,0 +1,35 @@
+const sketchWidth = 400;
+const sketchHeight = 400;
+
+
+function preload(){
+  // preload assets
+}
+
+
+function setup() {
+createCanvas(420, 600);
+
+}
+
+
+function draw() {
+background(255);
+noFill();
+strokeWeight(3);
+
+
+
+for (let y = 1; y < 50; y += 1) {
+  beginShape();
+for (let x = sketchWidth/40; x < sketchWidth; x += 1) {
+ 
+
+vertex(x, -50 + y * 20 + sin(Math.PI * 4 * (x / sketchWidth)) * 40);
+    
+}
+endShape();
+
+}
+
+}
